7227388b064597ddb2b1ba0d90d24850e6153a08
joehaines <joehaines@gmail.com>
2012-06-28 14:57:14 +0100

    changed to use foreman with port 5000

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,8 +12,9 @@ public class QueryProcessor {
         if (query.contains("hi")) {
             return "hello";
             //62694b40:%20what%20is%20your%20name
-        }  else  if (query.contains("what is your name")) {
-            return "joe";
+        }  else  if (query.contains("which of the following numbers is the largest")) {
+            String numbers_result[] = query.split(":");
+            System.out.println("Numbers:" + numbers_result[1]);
         }  else  if (query.contains("")) {
             return "";
         }  else  if (query.contains("")) {
