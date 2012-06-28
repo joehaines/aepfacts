b60c4ed5019a74fc6fd74a882e36594e3976d7b0
joehaines <joehaines@gmail.com>
2012-06-28 14:53:37 +0100

    changed to use foreman with port 5000

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,13 +7,25 @@ public class QueryProcessor {
         //output the request so that we can see it easily in the log
         System.out.println(query);
 
+
+
         if (query.contains("hi")) {
             return "hello";
             //62694b40:%20what%20is%20your%20name
         }  else  if (query.contains("what is your name")) {
             return "joe";
+        }  else  if (query.contains("")) {
+            return "";
+        }  else  if (query.contains("")) {
+            return "";
+        }  else  if (query.contains("")) {
+            return "";
+        }  else  if (query.contains("")) {
+            return "";
         }
-        return "";
+
+        //default response
+        return "I DON'T KNOW";
     }
 
 }
